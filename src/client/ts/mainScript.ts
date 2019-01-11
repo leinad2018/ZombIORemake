bc13573d2f578b3f49a5bafd752a2e714a98644d
leinad2018 <ishan.raikar@gmail.com>
2019-01-10 18:56:56 -0600

    added asset loading

--- src/client/ts/mainScript.ts
+++ src/client/ts/mainScript.ts
@@ -1,7 +1,26 @@
 import { ZIRCanvasController } from "CanvasController";
 import { ZIRTestClient } from "./unitTesting/TestClient";
+import { ZIRAssetLoader } from "./AssetLoader";
 var mainCanvas = document.getElementById("mainCanvas");
+
+loadAssets();
+var loaded = false;
+var id = setInterval(()=>{
+    loaded = ZIRAssetLoader.doneLoading();
+    console.log("Loading...");
+}, 100);
+
+while(!loaded){
+
+}
+
+clearInterval(id);
+
 var client: IZIRClient = new ZIRTestClient();
 var controller: ZIRCanvasController = new ZIRCanvasController(mainCanvas as HTMLCanvasElement, client);
 (client as ZIRTestClient).runTest();
 console.log("Test Ran");
+
+function loadAssets() {
+    ZIRAssetLoader.loadAsset("grass", "./assets/grass.png");
+}
