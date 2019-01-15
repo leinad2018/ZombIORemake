febd5d53fb7d625ff91cc44f84ed848282322d9a
leinad2018 <ishan.raikar@gmail.com>
2019-01-15 15:05:05 -0600

    added background movement

--- src/client/ts/CanvasController.ts
+++ src/client/ts/CanvasController.ts
@@ -1,16 +1,19 @@
 import { IZIRServerUpdate } from "./globalInterfaces/IServerUpdate";
 import { IZIRAsset, IZIRRenderable } from "./globalInterfaces/RenderingInterfaces";
 import { ZIRClientBase } from "./baseObjects/ClientBase";
+import { Point } from "./globalInterfaces/UtilityInterfaces";
 
 export class ZIRCanvasController implements IZIRServerUpdate {
     private canvas: HTMLCanvasElement;
     private client: ZIRClientBase;
     private DEBUG_RENDER: boolean = true;
+    private playerPosition: Point;
 
     constructor(canvas: HTMLCanvasElement, client: ZIRClientBase) {
         this.canvas = canvas;
         window.addEventListener("resize", this.handleResize.bind(this));
         this.client = client;
+        this.playerPosition = { x: 0, y: 0 };
         this.resizeWindow();
         this.render();
         this.client.registerUpdateHandler(this);
@@ -24,21 +27,14 @@ export class ZIRCanvasController implements IZIRServerUpdate {
     }
 
     private render() {
-        var ctx: CanvasRenderingContext2D = this.canvas.getContext('2d');
+        this.playerPosition = this.client.getPlayerPosition();
+        let ctx: CanvasRenderingContext2D = this.canvas.getContext('2d');
 
-        var background: IZIRAsset = this.client.getBackgroundImage();
+        let background: IZIRAsset = this.client.getBackgroundImage();
         this.renderBackground(ctx, background);
 
-        let playerPosition = this.client.getPlayerPosition();
-        let xOffset = this.canvas.width / 2 - playerPosition.x;
-        let yOffset = this.canvas.height / 2 - playerPosition.y;
-
-        ctx.save();
-        ctx.setTransform(1, 0, 0, 1, xOffset, yOffset);
-
-        var entities: IZIRRenderable[] = this.client.getEntitiesToRender();
+        let entities: IZIRRenderable[] = this.client.getEntitiesToRender();
         this.renderEntities(ctx, entities);
-        ctx.restore();
 
         this.renderPlayerBox(ctx, this.client.getPlayersOnline());
         if (this.DEBUG_RENDER) this.renderDebugBox(ctx, this.client.getDebugMessages());
@@ -50,7 +46,7 @@ export class ZIRCanvasController implements IZIRServerUpdate {
         ctx.fillRect(this.canvas.width - 150, 0, 150, players.length * 10 + 30);
         ctx.fillStyle = "white";
         ctx.fillText("Players Online:\n", this.canvas.width - 145, 15);
-        for (var i = 0; i < players.length; i++) {
+        for (let i = 0; i < players.length; i++) {
             ctx.fillText(players[i], this.canvas.width - 145, 25 + i * 10);
         }
 
@@ -65,7 +61,7 @@ export class ZIRCanvasController implements IZIRServerUpdate {
         ctx.fillRect(0, vOffset, this.canvas.width, boxHeight);
         ctx.fillStyle = "white";
         ctx.fillText("Debug:\n", 5, vOffset + 15);
-        for (var i = 0; i < messages.length; i++) {
+        for (let i = 0; i < messages.length; i++) {
             ctx.fillText(messages[i], 5, vOffset + 25 + i * 10);
         }
 
@@ -74,14 +70,27 @@ export class ZIRCanvasController implements IZIRServerUpdate {
 
     private renderBackground(ctx: CanvasRenderingContext2D, image: IZIRAsset) {
         ctx.save();
-        var background = image.getImage();
-        var pattern = ctx.createPattern(background, 'repeat');
+        let background = image.getImage();
+        let pattern = ctx.createPattern(background, 'repeat');
         ctx.fillStyle = pattern;
-        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        let bkWidth = background.width;
+        let bkHeight = background.height;
+        let offsetX = this.playerPosition.x % bkWidth;
+        let offsetY = this.playerPosition.y % bkHeight;
+
+        ctx.setTransform(1, 0, 0, 1, offsetX, offsetY);
+        ctx.fillRect(-bkWidth, -bkHeight, this.canvas.width + bkWidth * 2, this.canvas.height + bkHeight * 2);
         ctx.restore();
     }
 
     private renderEntities(ctx: CanvasRenderingContext2D, entities: IZIRRenderable[]) {
+        let xOffset = this.canvas.width / 2 - this.playerPosition.x;
+        let yOffset = this.canvas.height / 2 - this.playerPosition.y;
+
+        ctx.save();
+        ctx.setTransform(1, 0, 0, 1, xOffset, yOffset);
+
         for (let entity of entities) {
             let asset = entity.getImageToRender();
             let position = entity.getPosition();
@@ -89,6 +98,8 @@ export class ZIRCanvasController implements IZIRServerUpdate {
             position.y -= asset.getImage().height / 2;
             ctx.drawImage(asset.getImage(), position.x, position.y);
         }
+
+        ctx.restore();
     }
 
     public resizeWindow() {
